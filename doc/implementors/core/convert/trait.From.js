1303b34396b1fa4105f0af7f6257c58cbd0af70e
Jonas Hagstedt <hagstedt@gmail.com>
2018-11-18 15:50:14 +0100

    doc updates

---
+++ doc/implementors/core/convert/trait.From.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["sonr"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"https://docs.rs/mio/0.6.16/mio/net/tcp/struct.TcpStream.html\" title=\"struct mio::net::tcp::TcpStream\">TcpStream</a>&gt; for <a class=\"struct\" href=\"sonr/connections/struct.TcpConnection.html\" title=\"struct sonr::connections::TcpConnection\">TcpConnection</a>",synthetic:false,types:["sonr::connections::tcp_connection::TcpConnection"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"https://docs.rs/mio-uds/0.6/mio_uds/stream/struct.UnixStream.html\" title=\"struct mio_uds::stream::UnixStream\">UnixStream</a>&gt; for <a class=\"struct\" href=\"sonr/connections/struct.UdsConnection.html\" title=\"struct sonr::connections::UdsConnection\">UdsConnection</a>",synthetic:false,types:["sonr::connections::uds_connection::UdsConnection"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/std/io/error/struct.Error.html\" title=\"struct std::io::error::Error\">Error</a>&gt; for <a class=\"enum\" href=\"sonr/errors/enum.Error.html\" title=\"enum sonr::errors::Error\">Error</a>",synthetic:false,types:["sonr::errors::Error"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ doc/implementors/std/io/trait.Write.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["sonr"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a> for <a class=\"struct\" href=\"sonr/connections/struct.TcpConnection.html\" title=\"struct sonr::connections::TcpConnection\">TcpConnection</a>",synthetic:false,types:["sonr::connections::tcp_connection::TcpConnection"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a> for <a class=\"struct\" href=\"sonr/connections/struct.UdsConnection.html\" title=\"struct sonr::connections::UdsConnection\">UdsConnection</a>",synthetic:false,types:["sonr::connections::uds_connection::UdsConnection"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
